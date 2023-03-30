a8dd78f46931b7ec106ebc1a65ad09818a95f4d4
Tanuja755 <tanujarawattanujarawat755@gmail.com>
2023-03-30 15:04:52 +0530

    initial msg

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import "./index.css"; 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+var v=[1,2,3,4,5,6,7,8];
+var h=["a","b","c","d","e","f","g","h"];
+
+function App(){
+	
+	let board=[];
+	for(let j=v.length-1; j>=0;j--){
+	for(let i=0; i <h.length;i++){
+	var number=j +i+2;
+	if(number%2==0){
+		board.push(
+	<div className="white">
+	</div>
+	);
+	}
+   else{
+   board.push(
+   <div className="black">
+   </div>
+	);
+   }
+	}
+	}
+	return(
+	 <React.Fragment>
+	 <div id="p">CHESS BOARD</div>
+	 <div id="chessboard">{board}</div>
+	 </React.Fragment>
+	);
+
+
+}
+ReactDOM.render(
+App()
+,document.getElementById('root'));
